f99352f4e59526b0652b5f5b26b7303aa0f4ac64
KuanchaoChu <38451308+KuanChaoCHU@users.noreply.github.com>
2019-10-29 18:29:46 +0900

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 18:25:07 2019
+@author: KuanChao Chu
+
+Topic:
+"""
+
+import time 
+
+
+def print_time():
+    print(time.time())
+    return 
+
+print_time()
